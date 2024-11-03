057cae1eca45e247634bf49c9749fbdf82d6e2c3
itsmecyril <cyrilypil@gmail.com>
2024-11-04 00:13:53 +0800

    removed def login() in app.py, added w3.css again

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
 @app.route('/')
 def index():
     return render_template('Landing.html')
